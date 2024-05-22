77d48304ed6e1facf707f58838242a2f1d804547
arefa313 <arefa.noori.1380@gmail.com>
2024-05-22 12:46:11 +0430

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+let myName= "arefa";
+let array = [1,2,3,4];
+root.render(
+<>
+ <header>header</header>
+ <section>
+  <div className="row">
+    <div className="col">
+      <img src="" alt="" />
+    </div>
+  </div>
+ </section>
+ <footer>footer</footer>
+</>
+);
+
+
